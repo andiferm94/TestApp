c9823120d6275eedd8b1553123f393b400c014dd
andiferm <miguel.diaz47@std.uni.edu.ni>
2024-08-06 18:25:21 -0600

    Add project files.

---
+++ TestApp/Controllers/ClientController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestApp.ServiceReference1;
+
+namespace TestApp.Controllers
+{
+    public class ClientController : Controller
+    {
+
+        //Ejemplos de almacenamiento de cache, para mejorar rendimiento en la vista donde se muestra cantidades grandes informacion
+        private const string CacheKeyClientes = "ClientesCache";
+        private const string CacheKeyDirecciones = "DireccionesCache";
+        private const int CacheDurationMiliSegundos = 30; 
+
+        // GET: Client
+        public ActionResult Index()
+        {
+            // Obtener datos de caché
+            var clientes = HttpRuntime.Cache[CacheKeyClientes] as List<vw_clientes>;
+            var direcciones = HttpRuntime.Cache[CacheKeyDirecciones] as List<vw_direccion>;
+
+            // Si no están en caché, obtener de la fuente de datos y almacenar en caché
+            if (clientes == null || direcciones == null)
+            {
+                using (var client = new ClienteClient())
+                {
+                    clientes = client.ObtenerClientes().ToList();
+                    direcciones = client.ObtenerDireccion().ToList();
+
+                    // Almacenar en caché
+                    HttpRuntime.Cache.Insert(CacheKeyClientes, clientes, null, DateTime.Now.AddMilliseconds(CacheDurationMiliSegundos), TimeSpan.Zero);
+                    HttpRuntime.Cache.Insert(CacheKeyDirecciones, direcciones, null, DateTime.Now.AddMilliseconds(CacheDurationMiliSegundos), TimeSpan.Zero);
+                }
+            }
+
+            // Pasar datos a la vista
+            ViewBag.List = clientes;
+            ViewBag.cbdireccion = direcciones;
+
+            return View();
+        }
+
+        public ActionResult Modal()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AccionCliente(FormCollection frm)
+        {
+            try
+            {
+                var id = frm["id"];
+                var txtIdentificacion = frm["txtidentificacion"].Trim();
+                var cbTipo = frm["cbtipo"];
+                var txtPrimerNombre = frm["txtprimer_nombre"].Trim();
+                var txtSegundoNombre = frm["txtsegundo_nombre"].Trim();
+                var txtPrimerApellido = frm["txtprimer_apellido"].Trim();
+                var txtSegundoApellido = frm["txtsegundo_apellido"].Trim();
+                var cbDireccion = frm["cbdireccion"];
+
+                using (var client = new ClienteClient())
+                {
+                    if (string.IsNullOrWhiteSpace(id))
+                    {
+                        client.Alta(txtIdentificacion, cbTipo, txtPrimerNombre, txtSegundoNombre, txtPrimerApellido, txtSegundoApellido, int.Parse(cbDireccion));
+                    }
+                    else
+                    {
+                        client.Modificar(int.Parse(id), txtIdentificacion, cbTipo, txtPrimerNombre, txtSegundoNombre, txtPrimerApellido, txtSegundoApellido, int.Parse(cbDireccion));
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores
+                ViewBag.ErrorMessage = "Error al procesar la solicitud. Inténtelo nuevamente." + ex.Message;
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public ActionResult BajaCliente(string id)
+        {
+            try
+            {
+                using (var client = new ClienteClient())
+                {
+                    client.Baja(int.Parse(id));
+                }
+
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores
+                ViewBag.ErrorMessage = "Error al dar de baja el cliente. Inténtelo nuevamente." + ex.Message;
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+    }
+}
---
+++ TestApp/Scripts/cliente/funciones.js
@@ -0,0 +1,131 @@
+﻿$(document).ready(function () {
+
+    $("#frmCliente").validate({
+        rules: {
+            txtidentificacion: {
+                required: true,
+                maxlength: 14,
+                maxlength: 14
+            },
+            cbtipo: {
+                required: true
+            },
+            txtprimer_nombre: {
+                required: true,
+                maxlength: 50
+            },
+            txtprimer_apellido: {
+                required: true,
+                maxlength: 50
+            },
+            cbdireccion: {
+                required: true
+            }
+        },
+        messages: {
+            txtidentificacion: {
+                required: "Por favor, ingrese una identificación.",
+                minlength: "La identificación debe tener minimo 14 caracteres."
+
+                maxlength: "La identificación debe tener máximo 16 caracteres."
+            },
+            cbtipo: {
+                required: "Por favor, seleccione un tipo de identificación."
+            },
+            txtprimer_nombre: {
+                required: "Por favor, ingrese el primer nombre.",
+                maxlength: "El primer nombre debe tener máximo 50 caracteres."
+            },
+            txtprimer_apellido: {
+                required: "Por favor, ingrese el primer apellido.",
+                maxlength: "El primer apellido debe tener máximo 50 caracteres."
+            },
+            cbdireccion: {
+                required: "Por favor, seleccione una dirección."
+            }
+        },
+        submitHandler: function (form) {
+            var oForm = $(form).serialize();
+            $.ajax({
+                type: "POST",
+                url: "/Client/AccionCliente",
+                data: oForm,
+                success: function () {
+                    window.location.reload(); // Recargar la página
+                },
+                error: function (xhr, status, error) {
+                    console.error('Error en la solicitud AJAX:', error);
+                }
+            });
+        }
+    });
+
+    function resetForm() {
+        $('#frmCliente')[0].reset();
+        $('#frmCliente input, #frmCliente select').removeAttr('readonly');
+        $('#btguardar').removeAttr('disabled');
+    }
+
+    function disableForm() {
+        $('#frmCliente input, #frmCliente select').attr('readonly', true);
+        $('#btguardar').attr('disabled', true);
+    }
+
+    function enableForm() {
+        $('#frmCliente input, #frmCliente select').attr('readonly', false);
+        $('#btguardar').attr('disabled', false);
+    }
+
+    $('#btn-agregar').click(function () {
+        resetForm();
+    });
+
+    $('.btnedit').click(function () {
+        var data = $(this).data();
+        $("#id").val(data.id);
+        $("#txtidentificacion").val(data.identificacion);
+        $("#cbtipo").val(data.tipo).change();
+        $("#txtprimer_nombre").val(data.pnombre);
+        $("#txtsegundo_nombre").val(data.snombre);
+        $("#txtprimer_apellido").val(data.papellido);
+        $("#txtsegundo_apellido").val(data.sapellido);
+        $("#cbdireccion").val(data.cbdireccion).change();
+        $("#txtestado").val(data.estado);
+
+        if (data.estado === 'Inactivo') {
+            disableForm();
+        } else {
+            enableForm();
+        }
+    });
+ 
+    $(".btnbaja").click(function () {
+        var clientId = $(this).data('id'); // Obtén el ID del botón
+
+        Swal.fire({
+            title: "¿Esta seguro que desea dar de baja a este cliente?",
+            showDenyButton: true,
+            confirmButtonText: "SI",
+            denyButtonText: `No`
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    type: "POST",
+                    url: "/Client/BajaCliente",
+                    data: JSON.stringify({ id: clientId }),
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    success: function (response) {
+                        if (response.success) {
+                            window.location.reload(); // Recargar la página
+                        } else {
+                            Swal.fire('Error', response.message, 'error');
+                        }                    },
+                    error: function (xhr, status, error) {
+                        console.error('Error en la solicitud AJAX:', error);
+                    }
+                });
+            }
+        });
+    });
+});
